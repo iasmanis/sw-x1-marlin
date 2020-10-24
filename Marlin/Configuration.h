3275ced16164ab8a1e0908685160c7da6e446da6
Ingemars Asmanis <ingemars.asmanis@notakey.com>
2020-10-24 14:09:08 +0300

    Increase TEMP_HYSTERESIS for tool

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -444,7 +444,7 @@
 
 #define TEMP_RESIDENCY_TIME     10  // (seconds) Time to wait for hotend to "settle" in M109
 #define TEMP_WINDOW              1  // (°C) Temperature proximity for the "temperature reached" timer
-#define TEMP_HYSTERESIS          5  // (°C) Temperature proximity considered "close enough" to the target
+#define TEMP_HYSTERESIS          7  // (°C) Temperature proximity considered "close enough" to the target
 
 #define TEMP_BED_RESIDENCY_TIME 10  // (seconds) Time to wait for bed to "settle" in M190
 #define TEMP_BED_WINDOW          1  // (°C) Temperature proximity for the "temperature reached" timer
