96aa8d37d953c33216c2654f542407c96faa47ff
Ingemārs <ingemars.asmanis@notakey.com>
2022-05-22 11:20:10 +0300

    Update Configuration.h
    Add updated PID tune values

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -499,9 +499,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp  14.58
-    #define DEFAULT_Ki   1.14
-    #define DEFAULT_Kd 46.57
+    #define DEFAULT_Kp 23.64
+    #define DEFAULT_Ki 2.65
+    #define DEFAULT_Kd 52.78
   #endif
 #endif // PIDTEMP
 
