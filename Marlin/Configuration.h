acd2821801abb667755cc2dadb10a1b131716aaf
Ingemars Asmanis <ingemars.asmanis@notakey.com>
2023-12-01 11:20:42 +0200

    Reduce MINTEMP

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -470,15 +470,15 @@
 
 // Below this temperature the heater will be switched off
 // because it probably indicates a broken thermistor wire.
-#define HEATER_0_MINTEMP 1
-#define HEATER_1_MINTEMP 1
-#define HEATER_2_MINTEMP 1
-#define HEATER_3_MINTEMP 1
-#define HEATER_4_MINTEMP 1
-#define HEATER_5_MINTEMP 1
-#define HEATER_6_MINTEMP 1
-#define HEATER_7_MINTEMP 1
-#define BED_MINTEMP 1
+#define HEATER_0_MINTEMP -10
+#define HEATER_1_MINTEMP -10
+#define HEATER_2_MINTEMP -10
+#define HEATER_3_MINTEMP -10
+#define HEATER_4_MINTEMP -10
+#define HEATER_5_MINTEMP -10
+#define HEATER_6_MINTEMP -10
+#define HEATER_7_MINTEMP -10
+#define BED_MINTEMP -10
 
 // Above this temperature the heater will be switched off.
 // This can protect components from overheating, but NOT from shorts and failures.
