a9df6f07f1e19169ff50b295c61d8320d28b0cc5
Ingemars Asmanis <ingemars.asmanis@notakey.com>
2023-12-04 10:50:31 +0200

    Mintemp

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -628,7 +628,7 @@
 
 // Below this temperature the heater will be switched off
 // because it probably indicates a broken thermistor wire.
-#define HEATER_0_MINTEMP 5
+#define HEATER_0_MINTEMP -10
 #define HEATER_1_MINTEMP 5
 #define HEATER_2_MINTEMP 5
 #define HEATER_3_MINTEMP 5
@@ -636,7 +636,7 @@
 #define HEATER_5_MINTEMP 5
 #define HEATER_6_MINTEMP 5
 #define HEATER_7_MINTEMP 5
-#define BED_MINTEMP 5
+#define BED_MINTEMP -10
 #define CHAMBER_MINTEMP 5
 
 // Above this temperature the heater will be switched off.
